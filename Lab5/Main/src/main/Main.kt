4a0dc6c518c59ebdd937982226082d2d07a0675b
Sc00taloo <v4nyapop0v12@gmail.com>
2024-05-17 15:36:22 +0300

    Лаба 5 Задание 3 Вариант 2
    Были построены 6 функци1, решающих задачи предыдущего задания с помощью рекурсии вверх
    и хвостовой рекурсии. Все функции были протестированы.

--- Lab5/Main/src/main/Main.kt
+++ Lab5/Main/src/main/Main.kt
@@ -12,4 +12,26 @@ class Main {
     //кол-во делителей числа
     fun numbNumb(n: Int, k: Int, x: Int): Int = if (n == x) k+1 else if (n % x == 0) numbNumb(n, k+1, x+1) else numbNumb(n, k, x+1)
     fun numb(n: Int): Int = numbNumb(n,0, 1)
+
+    //максимальная цифра числа рекурсией вниз
+    fun maxxDown(x: Int, a: Int): Int = if(x%10 == 0) if(a < x) x else a else if (x%10 < a) maxx(x/10, a) else  maxx(x/10, x%10)
+    fun maxDown(x: Int): Int = maxxDown(x, 0)
+
+    //максимальная цифра числа рекурсией вверх
+    fun maxUp(x: Int): Int = if (x < 10) x else if (maxUp(x/10) > x%10) maxUp(x/10) else x%10
+
+    //сумма цифр числа делящихся на 3 рекурсией вверх
+    fun sum3Up(n: Int): Int = if (n % 10 == 0) 0 else if ((n%10) % 3 == 0) n%10 + sum3Up(n/10) else sum3Up(n/10)
+
+    //сумма цифр числа делящихся на 3 рекурсией вниз
+    fun sum33Down(n: Int, k: Int): Int = if (n % 10 == 0) k else if ((n%10) % 3 == 0) sum33Down(n/10, k+(n%10)) else sum33Down(n/10, k)
+    fun sum3Down(n: Int): Int = sum33Down(n, 0)
+
+    //кол-во делителей числа рекурсией вниз
+    fun numbDownn(n: Int, k: Int, x: Int): Int = if (n == x) k+1 else if (n % x == 0) numbDownn(n, k+1, x+1) else numbDownn(n, k, x+1)
+    fun numbDown(n: Int): Int = numbDownn(n,0, 1)
+
+    //кол-во делителей числа рекурсией вверх
+    fun numbUpp(x: Int, k: Int): Int = if (k > x) 0 else (if (x % k == 0) 1 + numbUpp(x, k+1) else 0 + numbUpp(x, k+1))
+    fun numbUp(x: Int): Int = numbUpp(x,1)
 }
--- Lab5/Main/src/test/test.kt
+++ Lab5/Main/src/test/test.kt
@@ -22,4 +22,46 @@ internal class Test {
         val expected = 8
         assertEquals(expected, main.numb(24))
     }
+
+    @Test
+    fun maxDown() {
+        val main = Main()
+        val expected = 7
+        assertEquals(expected, main.maxDown(471742))
+    }
+
+    @Test
+    fun maxUp() {
+        val main = Main()
+        val expected = 7
+        assertEquals(expected, main.maxUp(471742))
+    }
+
+    @Test
+    fun sum3Up() {
+        val main = Main()
+        val expected = 15
+        assertEquals(expected, main.sum3Up(1234566))
+    }
+
+    @Test
+    fun sum3Down() {
+        val main = Main()
+        val expected = 15
+        assertEquals(expected, main.sum3Down(1234566))
+    }
+
+    @Test
+    fun numbDown() {
+        val main = Main()
+        val expected = 8
+        assertEquals(expected, main.numbDown(24))
+    }
+
+    @Test
+    fun numbUp() {
+        val main = Main()
+        val expected = 8
+        assertEquals(expected, main.numbUp(24))
+    }
 }
